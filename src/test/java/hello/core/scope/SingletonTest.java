80019b003c08850e0fd3e3fea3fbec924101a472
NamCheolKim <pkeowkd@naver.com>
2021-03-15 22:06:41 +0900

    spring-core-principles-basic;

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind(){
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = annotationConfigApplicationContext.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = annotationConfigApplicationContext.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        annotationConfigApplicationContext.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+
+    }
+}
